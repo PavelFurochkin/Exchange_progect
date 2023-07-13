9c0189715dd04023d6d944c30b27af8926097682
Павел <sunshine4258@gmail.com>
2023-07-13 22:43:15 +0300

    Test server

--- Execute_from_db.py
+++ Execute_from_db.py
@@ -1,13 +1,35 @@
 import sqlite3
 
 
-class ExecuteBd:
+class ExecuteBdCurrency:
+    def __init__(self, data_base, table):
+        self.data_base = data_base
+        self.table = table
 
-    conn = sqlite3.connect('Exchange_base.db')
-    cur = conn.cursor()
+    def execute(self):
+        conn = sqlite3.connect(self.data_base)
+        cur = conn.cursor()
 
-    # Выполняем запрос для получения всех записей из таблицы Currencies
-    cur.execute("SELECT * FROM Currencies")
-    rows = cur.fetchall()
+        # Выполняем запрос для получения всех записей из таблицы Currencies
+        cur.execute(f'SELECT * FROM {self.table}')
+        rows = cur.fetchall()
 
-    conn.close()
+        conn.close()
+        return rows
+
+
+class Currency:
+    def __init__(self, id_table, code, full_name, sign):
+        self.id = id_table
+        self.code = code
+        self.full_name = full_name
+        self.sign = sign
+
+    def currency_representation(self):
+        item = {
+            'ID': self.id,
+            'Code': self.code,
+            'FullName': self.full_name,
+            'Sign': self.sign
+        }
+        return item
--- server.py
+++ server.py
@@ -13,13 +13,26 @@ Hello = HelloWorld('world')
 
 
 class OurHandler(BaseHTTPRequestHandler):
+    # Метод обрабатывает каждую валюту из таблицы
     def currencies(self):
+        data = Ex_db.ExecuteBdCurrency('Exchange_base.db', 'Currencies').execute()
+        currencies_list = []
+        for currency in data:
+            current_currency = Ex_db.Currency(*currency)
+            currency_dict = {
+                'id': current_currency.id,
+                'name': current_currency.full_name,
+                'code': current_currency.code,
+                'sign': current_currency.sign
+            }
+            currencies_list.append(currency_dict)
+
         self.send_response(HTTPStatus.OK)
-        self.send_header('Content-Type', 'application/json; charset=utf-8')
+        self.send_header('Content-Type', 'application/json; charset=UTF-8')
         self.end_headers()
 
-        json_st = json.dumps(Ex_db.ExecuteBd.rows, ensure_ascii=False)
-        self.wfile.write(json_st.encode('utf-8'))
+        response = json.dumps(currencies_list, indent=4, ensure_ascii=False)
+        self.wfile.write(response.encode('utf-8'))
 
     def hello_page(self):
         self.send_response(HTTPStatus.OK)
